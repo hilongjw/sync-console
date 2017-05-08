9d40d9088e12935e9db5382bd18c76cf330cc30f
Awe <hilongjw@gmail.com>
2017-05-08 10:34:38 +0800

    remove: flag reg

--- client/trace/sync-console/flag.js
+++
@@ -1,9 +0,0 @@
-import { getParams } from '../utils'
-export default class Flag {
-    constructor () {
-        const query = getParams()
-        console.debug(query)
-        this.token = query._sync_console_token
-        this.show = query._sync_console_show
-    }
-}
