6c5a0bda842840b48fb4d2a7805a3ecde10e37ce
Awe <hilongjw@gmail.com>
2017-05-15 11:50:36 +0800

    add: log type filter

--- build/sync-to-server.sh
+++ build/sync-to-server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-rm ../../github/sync-console-server/static/client/sync-console*.js
-cp public/client/sync-console*.js ../../github/sync-console-server/static/client/
+echo "start copy build file to server"
+
+server_path="../../github/sync-console-server/static/client/"
+client_path="public/client/"
+file_path="sync-console*.js"
+
+rm $server_path$file_path
+cp $client_path$file_path $server_path
+
+echo "copy success"
